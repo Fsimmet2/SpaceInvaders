e9454e5547f39bbe20ac2f37b75929da1a0ea8cf
Fsimmet2 <floriane.s@codeur.online>
2017-10-25 08:24:21 +0200

    fichier js

--- jsf.js
+++ jsf.js
@@ -4,14 +4,17 @@
 
       menu.style.display = "none";
       jeu.style.display = "block";
+      myMove();
   }
 
   var l = window.innerWidth;
   var h = window.innerHeight;
   var elem = document.querySelector("#joueur");
-  console.log(h);
+  console.log(l);
   window.addEventListener("keydown", moveLeft);
   window.addEventListener("keydown", moveRight);
+
+
   function moveLeft(e){
     if (e.keyCode == "37"){
         var elle = getComputedStyle(joueur).marginLeft;
@@ -28,7 +31,7 @@
   function moveRight(e){
     if (e.keyCode == "39" ){
         var ele = getComputedStyle(joueur).marginLeft;
-        if (parseInt(ele) <= l - 95){
+        if (parseInt(ele) <=  l - 95){
          var x = (parseInt(ele) + 10);
          console.log(x);
          elem.style.marginLeft = x + "px";
@@ -37,73 +40,73 @@
   }
   }
 
+  var elem = document.querySelector("#langage");
+  console.log(elem.getBoundingClientRect().top);
+
+  function myMove() {
+
+    var x = getComputedStyle(langage).top;
+    var elem = document.querySelector("#langage");
+
+    console.log(x);
+    console.log(elem);
+
+    var pos = parseInt(x);
+    var id = setInterval(frame, 10);
+    var maxWidth = window.innerWidth;
 
 
-  window.onload = function(){
-    var max_width = window.innerWidth;
-    var max_height = window.innerHeight;
-    
-    var directions = {left:1,up:2,right:3,down:4}
-    var direction = getRandomDirection();
-    var distance = getRandomDistance();
-    
-    var target = document.querySelector("#langage");
-    var target_pos = {top:0,left:0}
-    
-    var i = 0;
-    
-    var render_rate = 10;
-    var move_step = 2;
-    
-    setInterval(function(){
-        i++;
-        if(i > distance){
-           distance = getRandomDistance();
-           direction = getRandomDirection();
-           i = 0;
-        }
-        move(target,direction,move_step)
-    },render_rate)
-    
-    function getRandomDistance(){
-        return Math.floor((Math.random() * 20) + 1) + 5;
+    function frame() {
+
+      if (pos == maxWidth - 545) {
+
+       clearInterval(id);
+       myMoveB(200, 1);
+
+      } else {
+        
+        pos++; 
+        elem.style.left = pos + 'px'; 
+      }
     }
-    
-    function getRandomDirection(){
-        return Math.floor((Math.random() * 4) + 1); }
-  
-        function move(el,direction,step){
-      switch(direction){
-         case directions.left: {
-           if(target_pos.left < max_width){
-               target_pos.left += step;
-               target.style.left = target_pos.left + "px";
-           }
-           break;
-         }
-         case directions.up: {
-           if(target_pos.top < max_height){
-               target_pos.top += step;
-               target.style.top = target_pos.top + "px";
-           }
-           break;
-         }
-          
-         case directions.right: {
-           if(target_pos.left > 0){
-              target_pos.left -= step;
-              target.style.left = target_pos.left + "px";
-           }
-           break;
-         }
-          
-         case directions.down: {
-           if(target_pos.top > 0){
-              target_pos.top -= step;
-              target.style.top = target_pos.top + "px";
-           }
-           break;
-         }
+  }
+
+  function myMoveB(y, nb) {
+    var x = getComputedStyle(langage).top;
+    var pos = parseInt(x);
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (pos == y) {
+       clearInterval(id);
+       if(nb == "1"){
+        myMoveL();
+       }else {
+         myMove();
+       }
+       
+      } else {
+        pos++; 
+        
+        elem.style.top = pos + 'px'; 
+      }
+    }
+  }
+
+  function myMoveL() {
+    console.log(elem);
+    var x = getComputedStyle(langage).left;
+    var maxWidth = window.innerWidth;
+    var pos = parseInt(x);
+    console.log(pos);
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (pos == 0) {
+       clearInterval(id);
+       myMoveB(400);
+      } else {
+        pos--; 
+        
+        elem.style.left = pos + 'px'; 
       }
     }
   }
